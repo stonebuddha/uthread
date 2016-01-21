0fb7700618c8ad69298236e7459c0d6e781ebd47
Di Wang <wayne.wangdi@yandex.com>
2016-01-21 23:09:04 +0800

    update test

--- test.c
+++ test.c
@@ -1,36 +1,33 @@
 #include <stdio.h>
 #include "uthread.h"
 
-long cnt = 0;
 uthread_mutex_t mut;
 
 void *thread(void *arg) {
-    int n = 100000;
     int i;
 
-    for (i = 0; i < n; ++ i) {
+    for (i = 0; i < 1000; ++ i) {
         uthread_mutex_lock(&mut);
-        cnt ++;
+        printf("Hello world! %ld\n", (long) arg);
         uthread_mutex_unlock(&mut);
     }
+
     return NULL;
 }
 
 int main(int argc, char *argv[]) {
-    uthread_t tids[10];
-    int i;
+    uthread_t tids[1023];
+    long i;
 
     uthread_mutex_init(&mut);
 
-    for (i = 0; i < 10; ++ i) {
-        uthread_create(&tids[i], thread, NULL);
+    for (i = 0; i < 1023; ++ i) {
+        uthread_create(&tids[i], thread, (void *) i);
     }
 
-    for (i = 0; i < 10; ++ i) {
+    for (i = 0; i < 1023; ++ i) {
         uthread_join(tids[i], NULL);
     }
-
-    printf("%ld\n", cnt);
 
     return 0;
 }
